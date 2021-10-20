cf0f6c6915b281e441f193846c58c5f18771dcdd
stephkri <stephane.krims@hotmail.ca>
2021-10-20 20:28:54 +0000

    Make users seeds, partly make quizzes seeds

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,7 @@
--- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+-- Users table seeds here
+INSERT INTO users (name, email, password) VALUES ('Alice', 'amenzel@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+INSERT INTO users (name, email, password) VALUES ('Kira', 'kirachankasingh@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+INSERT INTO users (name, email, password) VALUES ('Robert', 'r.b.obrien@hotmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+INSERT INTO users (name, email, password) VALUES ('Leon', 'leon.cribbins1982@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+INSERT INTO users (name, email, password) VALUES ('Jorge', 'jorgecarlosmendez@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+INSERT INTO users (name, email, password) VALUES ('Madeleine', 'mad-provencher@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
---
+++ db/seeds/02_quizzes.sql
@@ -0,0 +1,3 @@
+-- Quizzes table seeds here
+INSERT INTO quizzes (title, author_id) VALUES ('Central Asian Geography', 1);
+INSERT INTO quizzes (title, author_id) VALUES ('Frequencies in Music Theory', 2);
