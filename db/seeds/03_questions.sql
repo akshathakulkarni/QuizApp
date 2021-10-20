39b6c34b2362c440bae7f1a01f880cb559e20861
stephkri <stephane.krims@hotmail.ca>
2021-10-20 20:45:14 +0000

    Rebase to add query to questions table, continue questions seeds

--- db/seeds/03_questions.sql
+++ db/seeds/03_questions.sql
@@ -1,2 +1,3 @@
 -- Questions table seeds here
-INSERT INTO questions (quiz_id, correct_answer, wrong_1, wrong_2, wrong_3) VALUES (1, )
+INSERT INTO questions (quiz_id, query, correct_answer, wrong_1, wrong_2, wrong_3) VALUES (1, 'How many countries end with "-stan"?', '7', '5', '6', '8');
+INSERT INTO questions (quiz_id, query, correct_answer, wrong_1, wrong_2, wrong_3) VALUES (1, 'How many countries end with "-stan"?', '7', '5', '6', '8');
