274d3a035330c512d9ca851942469f7d89c3dd3f
stephkri <stephane.krims@hotmail.ca>
2021-10-20 20:33:47 +0000

    Finish quizzes seeds

--- db/seeds/02_quizzes.sql
+++ db/seeds/02_quizzes.sql
@@ -1,3 +1,10 @@
 -- Quizzes table seeds here
 INSERT INTO quizzes (title, author_id) VALUES ('Central Asian Geography', 1);
 INSERT INTO quizzes (title, author_id) VALUES ('Frequencies in Music Theory', 2);
+INSERT INTO quizzes (title, author_id, unlisted) VALUES ('Obscure Brass Facts', 3, true);
+INSERT INTO quizzes (title, author_id) VALUES ('Frequencies in Music Theory', 3);
+INSERT INTO quizzes (title, author_id, unlisted) VALUES ('Obscure Baroque String Facts', 4, true);
+INSERT INTO quizzes (title, author_id) VALUES ('The Double Bass', 4);
+INSERT INTO quizzes (title, author_id) VALUES ('Basic French', 5);
+INSERT INTO quizzes (title, author_id, unlisted) VALUES ('Fender Guitars', 6, true);
+INSERT INTO quizzes (title, author_id, unlisted) VALUES ('Gibson Guitars', 6, true);
