50250a8f832f7f4fb9c4fc6acaa6177741db1b74
stephkri <stephane.krims@hotmail.ca>
2021-10-21 05:47:41 +0000

    Complete attempts seeds

---
+++ db/seeds/05_attempts.sql
@@ -0,0 +1,8 @@
+-- Attempts seeds here
+INSERT INTO attempts (user_id, quiz_id, score, link) VALUES (1, 6, 2, 'ar9sfb');
+INSERT INTO attempts (user_id, quiz_id, score, link) VALUES (1, 8, 1, '346sg4');
+INSERT INTO attempts (user_id, quiz_id, score, link) VALUES (6, 2, 0, 'fbn30u');
+INSERT INTO attempts (user_id, quiz_id, score, link) VALUES (7, 4, 3, 'hfw0fh');
+INSERT INTO attempts (user_id, quiz_id, score, link) VALUES (2, 9, 2, '3459h2');
+INSERT INTO attempts (user_id, quiz_id, score, link) VALUES (5, 1, 1, '0sfb42');
+INSERT INTO attempts (user_id, quiz_id, score, link) VALUES (3, 7, 3, 'sf03ys');
