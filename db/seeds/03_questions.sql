cf1c45ebef205b6416bb31da24eb28fa8f93a37f
stephkri <stephane.krims@hotmail.ca>
2021-10-21 04:38:08 +0000

    Continue with questions seeds, trying to push properly

--- db/seeds/03_questions.sql
+++ db/seeds/03_questions.sql
@@ -1,3 +1,5 @@
 -- Questions table seeds here
 INSERT INTO questions (quiz_id, query, correct_answer, wrong_1, wrong_2, wrong_3) VALUES (1, 'How many countries end with "-stan"?', '7', '5', '6', '8');
-INSERT INTO questions (quiz_id, query, correct_answer, wrong_1, wrong_2, wrong_3) VALUES (1, 'How many countries end with "-stan"?', '7', '5', '6', '8');
+INSERT INTO questions (quiz_id, query, correct_answer, wrong_1, wrong_2, wrong_3) VALUES (1, 'Of these countries, which has the most land area?', 'Kazakhstan', 'Afghanistan', 'Pakistan', 'Uzbekistan');
+INSERT INTO questions (quiz_id, query, correct_answer, wrong_1, wrong_2, wrong_3) VALUES (1, 'Of these countries, which has the largest population?', 'Pakistan', 'Afghanistan', 'Kazakhstan', 'Uzbekistan');
+INSERT INTO questions (quiz_id, query, correct_answer, wrong_1, wrong_2, wrong_3) VALUES (2, 'Of those countries, which has the most land area?', 'Kazakhstan', 'Afghanistan', 'Pakistan', 'Uzbekistan');
