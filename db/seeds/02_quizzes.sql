b18d1e65c57bf270c7b3581b92c2cfeeb32a969c
stephkri <stephane.krims@hotmail.ca>
2021-10-21 05:35:15 +0000

    Finish questions seeds

--- db/seeds/02_quizzes.sql
+++ db/seeds/02_quizzes.sql
@@ -2,7 +2,7 @@
 INSERT INTO quizzes (title, author_id) VALUES ('Central Asian Geography', 1);
 INSERT INTO quizzes (title, author_id) VALUES ('Frequencies in Music Theory', 2);
 INSERT INTO quizzes (title, author_id, unlisted) VALUES ('Obscure Brass Facts', 3, true);
-INSERT INTO quizzes (title, author_id) VALUES ('Frequencies in Music Theory', 3);
+INSERT INTO quizzes (title, author_id) VALUES ('Regional Foods of Italy', 3);
 INSERT INTO quizzes (title, author_id, unlisted) VALUES ('Obscure Baroque String Facts', 4, true);
 INSERT INTO quizzes (title, author_id) VALUES ('The Double Bass', 4);
 INSERT INTO quizzes (title, author_id) VALUES ('Basic French', 5);
