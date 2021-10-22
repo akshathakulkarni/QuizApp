cb04edb89486cc922e4f16c61a243234dd5d8a96
stephkri <stephane.krims@hotmail.ca>
2021-10-22 03:34:06 +0000

    Add links to quizzes seeds

--- db/seeds/02_quizzes.sql
+++ db/seeds/02_quizzes.sql
@@ -1,10 +1,10 @@
 -- Quizzes table seeds here
-INSERT INTO quizzes (title, author_id) VALUES ('Central Asian Geography', 1);
-INSERT INTO quizzes (title, author_id) VALUES ('Frequencies in Music Theory', 2);
-INSERT INTO quizzes (title, author_id, unlisted) VALUES ('Obscure Brass Facts', 3, true);
-INSERT INTO quizzes (title, author_id) VALUES ('Regional Foods of Italy', 3);
-INSERT INTO quizzes (title, author_id, unlisted) VALUES ('Obscure Baroque String Facts', 4, true);
-INSERT INTO quizzes (title, author_id) VALUES ('The Double Bass', 4);
-INSERT INTO quizzes (title, author_id) VALUES ('Basic French', 5);
-INSERT INTO quizzes (title, author_id, unlisted) VALUES ('Fender Guitars', 6, true);
-INSERT INTO quizzes (title, author_id, unlisted) VALUES ('Gibson Guitars', 6, true);
+INSERT INTO quizzes (title, author_id, link) VALUES ('Central Asian Geography', 1, 'qsf2b0');
+INSERT INTO quizzes (title, author_id, link) VALUES ('Frequencies in Music Theory', 2, '2gfbe0');
+INSERT INTO quizzes (title, author_id, unlisted, link) VALUES ('Obscure Brass Facts', 3, true, 'wdgojn');
+INSERT INTO quizzes (title, author_id, link) VALUES ('Regional Foods of Italy', 3, '2g0bsn');
+INSERT INTO quizzes (title, author_id, unlisted, link) VALUES ('Obscure Baroque String Facts', 4, true, 'bv23f9');
+INSERT INTO quizzes (title, author_id, link) VALUES ('The Double Bass', 4, '3v4b30');
+INSERT INTO quizzes (title, author_id, link) VALUES ('Basic French', 5, '250623');
+INSERT INTO quizzes (title, author_id, unlisted, link) VALUES ('Fender Guitars', 6, true, 'qpvb40');
+INSERT INTO quizzes (title, author_id, unlisted, link) VALUES ('Gibson Guitars', 6, true, '3294gf');
