022acae4619056a2fa65ed82ef0a4c01fdb1034e
stephkri <stephane.krims@hotmail.ca>
2021-10-23 00:14:05 +0000

    Add makeNewQuestion file

--- routes/database.js
+++ routes/database.js
@@ -14,4 +14,22 @@ const makeNewQuiz = (data, user) => {
 
 module.exports = { makeNewQuiz };
 
+const makeNewQuestion = function(data) {
+  const quizID = data.quiz_id;
+  const query = data.query;
+  const correct = data.correct;
+  const wrong1 = data.wrong1;
+  const wrong2 = data.wrong2;
+  const wrong3 = data.wrong3;
+  const queryParams = [quizID, query, correct, wrong1, wrong2, wrong3];
+  const queryString = `INSERT INTO questions (quiz_id, query, correct_answer, wrong_1, wrong_2, wrong_3) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *;`;
+  return pool.query(queryString, queryParams)
+  .then((res) => {
+    const questionID = res.questions.id; //or whatever this ends up being
+    return pool.query(`INSERT INTO quizzes_questions (quiz_id, question_id) VALUES ($1, $2) RETURNING *;`,
+    [quizID, questionID])
+    .catch(e => console.log(e));
+  })
+}
 
+module.exports = { makeNewQuestion };
