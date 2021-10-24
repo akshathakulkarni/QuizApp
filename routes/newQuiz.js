217b4af279ac8ef6bfdcc321a4927517fd3d7f62
Akshatha Kulkarni <akshathakulkarni12@gmail.com>
2021-10-24 02:01:25 -0400

    Implement a post route for 'Create new quiz' and insert data into db

--- routes/newQuiz.js
+++ routes/newQuiz.js
@@ -47,37 +47,47 @@ module.exports = (db) => {
     const title = req.body.title;
     const author_id = req.session.user_id;
     console.log('author_id = ', author_id)
-    const unlisted = req.body.unlisted;
+    const unlisted = false;
     const link = generateRandomString(6, '8qy3zi');
     console.log('link = ', link)
     const query = req.body.query;
     const correct = req.body.correct;
     const wrong1 = req.body.wrong1;
     const wrong2 = req.body.wrong2;
-    const wrong3 = req.body.wrong3l
+    const wrong3 = req.body.wrong3;
     console.log('title : ', req.body)
-    // const query1 =`INSERT INTO quizzes (title, author_id, unlisted, link) VALUES ($1, $2, $3, $4) RETURNING *;`;
-    // const values1 = [title, author_id, unlisted, link];
-    // const query2 = `INSERT INTO questions (quiz_id, query, correct_answer, wrong_1, wrong_2, wrong_3) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *;`;
-    // const values2 = [quizID, query, correct, wrong1, wrong2, wrong3];
-    // db.query(query1, values1)
-    //   .then((res) => {
-    //     console.log(res.rows);
-    //     const quizId = res.rows.id; //Check for quizId
-    //     db.query(query2, values2)
-    //       .then((res) => {
-    //         res.json({ })
-    //       })
+    const query1 =`INSERT INTO quizzes (title, author_id, unlisted, link) VALUES ($1, $2, $3, $4) RETURNING *;`;
+    const values1 = [title, author_id, unlisted, link];
 
-    //   })
+    db.query(query1, values1)
+      .then(data => {
+        const newQuiz = data.rows;
+        const quiz_id = data.rows[0].id;
+        console.log('id = ', quiz_id)
+        console.log('result = ', newQuiz);
+        const query2 = `INSERT INTO questions (quiz_id, query, correct_answer, wrong_1, wrong_2, wrong_3) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *;`;
+        const values2 = [quiz_id, query, correct, wrong1, wrong2, wrong3];
+        db.query(query2, values2)
+          .then(data => {
+            const quizQuestionAnswers = data.rows;
+            const question_id = data.rows[0].id;
+            console.log('2 = ', quizQuestionAnswers);
+            const query3 = `INSERT INTO quizzes_questions (quiz_id, question_id) VALUES ($1, $2) RETURNING *;`;
+            const values3 = [quiz_id, question_id];
+            db.query(query3, values3)
+              .then(data => {
+                const quiz_question_ids = data.rows;
+                console.log('ids 3 = ', quiz_question_ids)
+                res.json({ quiz_question_ids });
+              })
 
-      // .then((data) => {
-      //   console.log('data = ' , data)
-      // })
-      // .catch((err) => {
-      //   console.log({ err: err.message })
-      // })
-    res.send(":-)")
+          })
+      })
+      .catch((err) => {
+        res
+          .status(500)
+          .json({ err: err.message });
+      })
 
   })
   return router;
