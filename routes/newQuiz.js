94f12aaad2ea732c3071f7380f5895a230d51bab
Akshatha Kulkarni <akshathakulkarni12@gmail.com>
2021-10-24 01:07:38 -0400

    Use generateRandomString function to generate unique link for each quiz

--- routes/newQuiz.js
+++ routes/newQuiz.js
@@ -30,6 +30,14 @@ const makeNewQuestion = function(data) {
   })
 }
 
+const generateRandomString = function(length, chars) {
+  let result = '';
+  for (let i = length; i > 0; i--) {
+    result += chars[Math.floor(Math.random() * chars.length)];
+  }
+  return result;
+};
+
 module.exports = (db) => {
   router.get('/', (req, res) => {
     res.render('dummynew');
@@ -38,28 +46,30 @@ module.exports = (db) => {
     console.log("inside newQuizPost")
     const title = req.body.title;
     const author_id = req.session.user_id;
+    console.log('author_id = ', author_id)
     const unlisted = req.body.unlisted;
-    const link = generateRandomString();
+    const link = generateRandomString(6, '8qy3zi');
+    console.log('link = ', link)
     const query = req.body.query;
     const correct = req.body.correct;
     const wrong1 = req.body.wrong1;
     const wrong2 = req.body.wrong2;
     const wrong3 = req.body.wrong3l
     console.log('title : ', req.body)
-    const query1 =`INSERT INTO quizzes (title, author_id, unlisted, link) VALUES ($1, $2, $3, $4) RETURNING *;`;
-    const values1 = [title, author_id, unlisted, link];
-    const query2 = `INSERT INTO questions (quiz_id, query, correct_answer, wrong_1, wrong_2, wrong_3) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *;`;
-    const values2 = [quizID, query, correct, wrong1, wrong2, wrong3];
-    db.query(query1, values1)
-      .then((res) => {
-        console.log(res.rows);
-        const quizId = res.rows.id; //Check for quizId
-        db.query(query2, values2)
-          .then((res) => {
-            res.json({ })
-          })
+    // const query1 =`INSERT INTO quizzes (title, author_id, unlisted, link) VALUES ($1, $2, $3, $4) RETURNING *;`;
+    // const values1 = [title, author_id, unlisted, link];
+    // const query2 = `INSERT INTO questions (quiz_id, query, correct_answer, wrong_1, wrong_2, wrong_3) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *;`;
+    // const values2 = [quizID, query, correct, wrong1, wrong2, wrong3];
+    // db.query(query1, values1)
+    //   .then((res) => {
+    //     console.log(res.rows);
+    //     const quizId = res.rows.id; //Check for quizId
+    //     db.query(query2, values2)
+    //       .then((res) => {
+    //         res.json({ })
+    //       })
 
-      })
+    //   })
 
       // .then((data) => {
       //   console.log('data = ' , data)
