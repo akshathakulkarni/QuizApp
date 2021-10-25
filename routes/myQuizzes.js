e0911a72b5c06d681ea2a868610e3adecb21900b
stephkri <stephane.krims@hotmail.ca>
2021-10-25 01:01:35 +0000

    My quizzes page now works for logged in users

--- routes/myQuizzes.js
+++ routes/myQuizzes.js
@@ -3,14 +3,24 @@ const router = express.Router();
 
 module.exports = (db) => {
   router.get('/', (req, res) => {
+    //console.log('Req session:', req.session);
+    //console.log('Req params:', req.params)
     author_id = req.session.user_id;
     console.log('author id = ', author_id);
-    db.query(`SELECT * FROM quizzes WHERE author_id = $1`, [author_id])
+    db.query(`SELECT quizzes.*, users.name FROM quizzes JOIN users ON users.id = quizzes.author_id WHERE quizzes.author_id = $1`, [author_id])
       .then(data => {
         const myQuizzes = data.rows;
         console.log('myQuizzes:', myQuizzes);
+        const userName = myQuizzes[0].name;
+        /*
+        const dataObj = {};
+        for (let q = 0; q < myQuizzes.length; q++) {
+          dataObj[q] = myQuizzes[q];
+        }
+        console.log('dataObj:', dataObj);
+        */
         //res.json({ myQuiz });
-        res.render("myquizzes", myQuizzes);
+        res.render("myquizzes", {'myQuizzes': myQuizzes, 'name': userName});
       })
       .catch((err) => {
         res
