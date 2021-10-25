a289eaeb93c42cb59dad784700ba8d6a86c7b2d7
stephkri <stephane.krims@hotmail.ca>
2021-10-25 20:38:53 +0000

    Make SQL route for attempt page

---
+++ routes/attemptpage.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const router = express.Router();
+
+module.exports = (db) => {
+  router.get('/:link', (req, res) => {
+    //console.log('Req session:', req.session);
+    //console.log('Req params:', req.params)
+    const link = req.params.link;
+    console.log('Cookie ID:', req.session.user_id);
+    console.log('We got here, req params', req.params);
+    db.query(`SELECT * FROM attempts WHERE link = $1`, [link])
+      .then(data => {
+        console.log('Got here in attempts', data.rows);
+        const attemptObj = data.rows[0];
+        db.query('SELECT name FROM users WHERE id = $1', [attemptObj.user_id])
+        .then(nameData => {
+          console.log('name data:', nameData.rows);
+          const name = nameData.rows[0].name;
+          db.query('SELECT title FROM quizzes WHERE id = $1', [attemptObj.quiz_id])
+          .then(quizData =>{
+            console.log('Quiz data:', quizData.rows);
+            const quizTitle = quizData.rows[0].title;
+            db.query('SELECT count(*) FROM questions WHERE quiz_id = $1', [attemptObj.quiz_id])
+            .then(questionCountData => {
+              console.log('question count', questionCountData.rows);
+              const questionCount = questionCountData.rows[0].count;
+              res.render('attemptpage', {
+                'attemptObj': attemptObj,
+                'name': name,
+                'quizTitle': quizTitle,
+                'questionCount': questionCount
+              })
+            })
+          })
+        })
+      })
+      .catch((err) => {
+        res
+          .status(500)
+          .json({ err: err.message });
+      })
+  })
+  return router;
+}
