f44b0ca738b929206f7d357eec9e6ecd9e60eec8
stephkri <stephane.krims@hotmail.ca>
2021-10-26 20:27:04 +0000

    Homepage not working, going to master to test

--- routes/quizList.js
+++ routes/quizList.js
@@ -4,28 +4,7 @@ const router = express.Router();
 module.exports = (db) => {
   router.get('/', (req, res) => {
     console.log('inside get route')
-    res.render('login');
-  })
-  router.post('/', (req, res) => {
-    console.log('inside post route')
-    const email = req.body.email;
-    const password = req.body.password;
-    db.query(`SELECT name, id FROM users WHERE email = $1 AND password = $2`, [email, password])
-      .then((data) => {
-        const name = data.rows[0].name;
-        req.session.user_id = data.rows[0].id;
-        console.log('id = ', req.session.user_id);
-        db.query(`SELECT quizzes.*, users.name FROM quizzes JOIN users ON users.id = quizzes.author_id WHERE unlisted = false`)
-        .then(data => {
-          console.log(data.rows);
-          res.render('index', { 'publicQuizzes': data.rows, 'name': name });
-        })
-      })
-      .catch((err) => {
-        res
-          .status(500)
-          .json({error: err.message});
-      });
+    res.render('index', { name });
   })
   return router;
 }
