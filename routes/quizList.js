de73af56555160f0e1b9f17258b2f925f223154f
stephkri <stephane.krims@hotmail.ca>
2021-10-26 21:29:49 +0000

    Make new route for quizList that can fit logged out users

--- routes/quizList.js
+++ routes/quizList.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 module.exports = (db) => {
   router.get('/', (req, res) => {
+    /*
     console.log('inside get route');
     const userId = req.session.user_id;
     db.query('SELECT name FROM users WHERE id = $1', [userId])
@@ -13,6 +14,22 @@ module.exports = (db) => {
       .then(quizData => {
         res.render('index', { 'publicQuizzes': quizData.rows, 'name': userName });
       })
+    })
+    .catch(e => console.log(e));
+    */
+    db.query('SELECT quizzes.*, users.name FROM quizzes JOIN users ON users.id = quizzes.author_id WHERE unlisted = false')
+    .then(quizData => {
+      const publicQuizzes = quizData.rows;
+      if (req.session.user_id) {
+        db.query('SELECT name FROM users WHERE id = $1', [userId])
+        .then(userData => {
+          console.log('name in home get:', userData.rows[0].name);
+          const userName = userData.rows[0].name;
+          res.render('index', { 'publicQuizzes': publicQuizzes, 'name': userName });
+        })
+      } else {
+        res.render('index', { 'publicQuizzes': publicQuizzes, 'name': null })
+      }
     })
     .catch(e => console.log(e));
   })
