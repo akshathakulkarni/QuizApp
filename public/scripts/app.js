885820ac2dbcb8854b5f88153492cdfddf7078c9
Akshatha Kulkarni <akshathakulkarni12@gmail.com>
2021-10-26 16:15:20 -0400

    Render the next question section dynamically using jQuery

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,60 +1,101 @@
 // Client facing scripts here
 
-const addQuestion = () => {
-  $addQuestion = `<section id='next-question'>
-  <form method="POST",  action='/api/newQuiz'>
-    <div class="input-group query">
-      <div class="input-group-prepend">
-        <span class="input-group-text" id="query-prompt">Question</span>
-      </div>
-      <input type="text" name="query" class="form-control" aria-describedby="query-prompt">
-    </div>
-    <div class="input-group correct">
-      <div class="input-group-prepend">
-        <span class="input-group-text" id="correct-prompt">Correct answer</span>
-      </div>
-      <input type="text" name="correct" class="form-control" aria-describedby="correct-prompt">
-    </div>
-    <div class="input-group wrong">
-      <div class="input-group-prepend">
-        <span class="input-group-text" id="wrong1-prompt">Wrong answer #1</span>
-      </div>
-      <input type="text" name="wrong1" class="form-control" aria-describedby="wrong1-prompt">
-    </div>
-    <div class="input-group wrong">
-      <div class="input-group-prepend">
-        <span class="input-group-text" id="wrong2-prompt">Wrong answer #2</span>
-      </div>
-      <input type="text" name="wrong2" class="form-control" aria-describedby="wrong2-prompt">
-    </div>
-    <div class="input-group wrong">
-      <div class="input-group-prepend">
-        <span class="input-group-text" id="wrong3-prompt">Wrong answer #3</span>
-      </div>
-      <input type="text" name="wrong3" class="form-control" aria-describedby="wrong3-prompt">
-    </div>
-    <br>
-    <br>
-</section>`
+const addQuestion = (count) => {
+  $addQuestion = `
+    <section class='next-question' data-count='${count}'>
+  <div class="input-group query">
+  <div class="input-group-prepend">
+    <span class="input-group-text" id="query-prompt">Question</span>
+  </div>
+  <input type="text" name="query" class="form-control" aria-describedby="query-prompt">
+</div>
+<div class="input-group correct">
+  <div class="input-group-prepend">
+    <span class="input-group-text" id="correct-prompt">Correct answer</span>
+  </div>
+  <input type="text" name="correct" class="form-control" aria-describedby="correct-prompt">
+</div>
+<div class="input-group wrong">
+  <div class="input-group-prepend">
+    <span class="input-group-text" id="wrong1-prompt">Wrong answer #1</span>
+  </div>
+  <input type="text" name="wrong1" class="form-control" aria-describedby="wrong1-prompt">
+</div>
+<div class="input-group wrong">
+  <div class="input-group-prepend">
+    <span class="input-group-text" id="wrong2-prompt">Wrong answer #2</span>
+  </div>
+  <input type="text" name="wrong2" class="form-control" aria-describedby="wrong2-prompt">
+</div>
+<div class="input-group wrong">
+  <div class="input-group-prepend">
+    <span class="input-group-text" id="wrong3-prompt">Wrong answer #3</span>
+  </div>
+  <input type="text" name="wrong3" class="form-control" aria-describedby="wrong3-prompt">
+</div>
+<br>
+<br>
+
+</section>
+`
 return $addQuestion;
 }
 
+const errorCheck = function(event) {
+  //event.preventDefault();
+  $error = $('#error');
+  $inputText = $('.form-control');
+
+  //For a valid input no error should be displayed.
+  $error.text("");
+  $error.removeClass('error');
+
+  //check if input content is empty
+  if($inputText.val() === '' || $inputText.val() === null || ($.trim($inputText.val()) === '' ))
+  {
+    $error.addClass('error');
+    $error.text("!!!Empty input! Please enter all questions and answers.");
+    $inputText.slideDown();
+    return true;
+  }
+  return false;
+}
 
 $(document).ready(function() {
 
   $(function() {
     const $button = $('#add-question');
+    let count = 0;
     $button.on('click', function () {
-    console.log('Button clicked, performing ajax call...');
-    $('#new-question').append(addQuestion());
+      count++;
+      console.log('Button clicked, performing ajax call...', count);
+      $('#question').append(addQuestion(count));
      });
   });
 
-  $('.new-quiz').submit(function(event) {
-    event.preventDefault();
-    console.log("Inside submit handler")
-    const serialisedData = $('form').serialize();
-    console.log('Data = ', serialisedData)
-  });
+  $('#quiz-form').submit(function(event) {
+    if((errorCheck(event))) {
+      event.preventDefault();
+    }
+  //   console.log('Innnnnn')
+  //   event.preventDefault();
+  //   $error = $('#error');
+  // $inputText = $('#question');
+
+  // //For a valid input no error should be displayed.
+  // $error.text("");
+  // $error.removeClass('error');
+
+  // //check if input content is empty
+  // if($inputText.val() === '' || $inputText.val() === null || ($.trim($inputText.val()) === '' ))
+  // {
+  //   console.log('error check')
+  //   $error.addClass('error');
+  //   $error.text("!!!Empty content! Please enter all questions and answers.");
+  //   $inputText.slideDown();
+  // }
+  //   console.log("Inside submit handler")
+  //   //console.log('Data = ', serialisedData)
+   });
 
 });
--- routes/newQuiz.js
+++ routes/newQuiz.js
@@ -51,49 +51,63 @@ module.exports = (db) => {
     const author_id = req.session.user_id;
     console.log('author_id = ', author_id)
     console.log('req = ', req.body)
-    console.log('req params = ', req.params)
+    const queryArray = req.body.query;
+    const correctAnswerArray = req.body.correct;
+    const wrongAnswer1Array = req.body.wrong1;
+    const wrongAnswer2Array = req.body.wrong2;
+    const wrongAnswer3Array = req.body.wrong3;
+    console.log(queryArray, correctAnswerArray, wrongAnswer1Array, wrongAnswer2Array, wrongAnswer3Array)
 
-    console.log(title, author_id, unlisted)
-    const link = generateRandomString(6, '8qy3zi');
-    console.log('link = ', link)
-    const query = req.body.query;
-    const correct = req.body.correct;
-    const wrong1 = req.body.wrong1;
-    const wrong2 = req.body.wrong2;
-    const wrong3 = req.body.wrong3;
-    console.log('title : ', req.body)
-    const query1 =`INSERT INTO quizzes (title, author_id, unlisted, link) VALUES ($1, $2, $3, $4) RETURNING *;`;
-    const values1 = [title, author_id, unlisted, link];
 
-    db.query(query1, values1)
-      .then(data => {
-        const newQuiz = data.rows;
-        const quiz_id = data.rows[0].id;
-        console.log('id = ', quiz_id)
-        console.log('result = ', newQuiz);
-        const query2 = `INSERT INTO questions (quiz_id, query, correct_answer, wrong_1, wrong_2, wrong_3) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *;`;
-        const values2 = [quiz_id, query, correct, wrong1, wrong2, wrong3];
-        db.query(query2, values2)
-          .then(data => {
-            const quizQuestionAnswers = data.rows;
-            const question_id = data.rows[0].id;
-            console.log('2 = ', quizQuestionAnswers);
-            const query3 = `INSERT INTO quizzes_questions (quiz_id, question_id) VALUES ($1, $2) RETURNING *;`;
-            const values3 = [quiz_id, question_id];
-            db.query(query3, values3)
-              .then(data => {
-                const quiz_question_ids = data.rows;
-                console.log('ids 3 = ', quiz_question_ids)
-                res.json({ quiz_question_ids });
-              })
+    for (let query of queryArray) {
+      for (let queryNumber = 0; queryNumber < queryArray.length; queryNumber++) {
+        let question
 
-          })
-      })
-      .catch((err) => {
-        res
-          .status(500)
-          .json({ err: err.message });
-      })
+      }
+
+    }
+
+    // console.log(title, author_id, unlisted)
+    // const link = generateRandomString(6, '8qy3zi');
+    // console.log('link = ', link)
+    // const query = req.body.query;
+    // const correct = req.body.correct;
+    // const wrong1 = req.body.wrong1;
+    // const wrong2 = req.body.wrong2;
+    // const wrong3 = req.body.wrong3;
+    // console.log('title : ', req.body)
+    // const query1 =`INSERT INTO quizzes (title, author_id, unlisted, link) VALUES ($1, $2, $3, $4) RETURNING *;`;
+    // const values1 = [title, author_id, unlisted, link];
+
+    // db.query(query1, values1)
+    //   .then(data => {
+    //     const newQuiz = data.rows;
+    //     const quiz_id = data.rows[0].id;
+    //     console.log('id = ', quiz_id)
+    //     console.log('result = ', newQuiz);
+    //     const query2 = `INSERT INTO questions (quiz_id, query, correct_answer, wrong_1, wrong_2, wrong_3) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *;`;
+    //     const values2 = [quiz_id, query, correct, wrong1, wrong2, wrong3];
+    //     db.query(query2, values2)
+    //       .then(data => {
+    //         const quizQuestionAnswers = data.rows;
+    //         const question_id = data.rows[0].id;
+    //         console.log('2 = ', quizQuestionAnswers);
+    //         const query3 = `INSERT INTO quizzes_questions (quiz_id, question_id) VALUES ($1, $2) RETURNING *;`;
+    //         const values3 = [quiz_id, question_id];
+    //         db.query(query3, values3)
+    //           .then(data => {
+    //             const quiz_question_ids = data.rows;
+    //             console.log('ids 3 = ', quiz_question_ids)
+    //             res.json({ quiz_question_ids });
+    //           })
+
+    //       })
+    //   })
+    //   .catch((err) => {
+    //     res
+    //       .status(500)
+    //       .json({ err: err.message });
+    //   })
 
   })
   return router;
