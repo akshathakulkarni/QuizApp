74f26801af2144b883c4ad68c5aa9545a004afda
Akshatha Kulkarni <akshathakulkarni12@gmail.com>
2021-10-27 12:32:42 -0400

    Store username in cookie session upon login

--- routes/login.js
+++ routes/login.js
@@ -14,6 +14,8 @@ module.exports = (db) => {
       .then((data) => {
         const name = data.rows[0].name;
         req.session.user_id = data.rows[0].id;
+        req.session.name = data.rows[0].name;
+        console.log('name = ', req.session.name)
         console.log('id = ', req.session.user_id);
         db.query(`SELECT quizzes.*, users.name FROM quizzes JOIN users ON users.id = quizzes.author_id WHERE unlisted = false`)
         .then(data => {
