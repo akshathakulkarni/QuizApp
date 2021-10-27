c10c032b8e325bf7131314eee7f5eae9fc5223cd
Akshatha Kulkarni <akshathakulkarni12@gmail.com>
2021-10-27 11:42:28 -0400

    Render a new to page which links to the newly created quiz after inserting

--- routes/newQuiz.js
+++ routes/newQuiz.js
@@ -2,36 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 
-
-const makeNewQuiz = function(data, user) {
-  const title = data.title;
-  const unlisted = data.unlisted;
-  const userID = user.id;
-  const link = generateRandomString();
-  return pool
-  .query(`INSERT INTO quizzes (title, author_id, unlisted, link) VALUES ($1, $2, $3, $4) RETURNING *`,
-  [title, userID, unlisted, link])
-  .catch(e => console.log(e));
-}
-
-const makeNewQuestion = function(data) {
-  const quizID = data.quiz_id;
-  const query = data.query;
-  const correct = data.correct;
-  const wrong1 = data.wrong1;
-  const wrong2 = data.wrong2;
-  const wrong3 = data.wrong3;
-  const queryParams = [quizID, query, correct, wrong1, wrong2, wrong3];
-  const queryString = `INSERT INTO questions (quiz_id, query, correct_answer, wrong_1, wrong_2, wrong_3) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *;`;
-  return pool.query(queryString, queryParams)
-  .then((res) => {
-    const questionID = res.questions.id; //or whatever this ends up being
-    return pool.query(`INSERT INTO quizzes_questions (quiz_id, question_id) VALUES ($1, $2) RETURNING *;`,
-    [quizID, questionID])
-    .catch(e => console.log(e));
-  })
-}
-
 const generateRandomString = function(length, chars) {
   let result = '';
   for (let i = length; i > 0; i--) {
@@ -39,6 +9,20 @@ const generateRandomString = function(length, chars) {
   }
   return result;
 };
+
+const dataArray = (obj) => {
+  let result = []
+    for(let i = 0; i < obj["query"].length; i++){
+        let tempObj = {}
+        tempObj["query"] = obj["query"][i]
+        tempObj["correct"] = obj["correct"][i]
+        tempObj["wrong1"] = obj["wrong1"][i]
+        tempObj["wrong2"] = obj["wrong2"][i]
+        tempObj["wrong3"] = obj["wrong3"][i]
+        result.push(tempObj)
+    }
+    return result
+}
 
 
 
@@ -50,21 +34,6 @@ module.exports = (db) => {
   router.post('/', (req, res) => {
     console.log("inside newQuizPost")
     console.log('req = ', req.body)
-
-    const dataArray = (obj) => {
-      let result = []
-        for(let i = 0; i < obj["query"].length; i++){
-            let tempObj = {}
-            tempObj["query"] = obj["query"][i]
-            tempObj["correct"] = obj["correct"][i]
-            tempObj["wrong1"] = obj["wrong1"][i]
-            tempObj["wrong2"] = obj["wrong2"][i]
-            tempObj["wrong3"] = obj["wrong3"][i]
-            result.push(tempObj)
-        }
-        return result
-    }
-
     let obj = req.body
     let dataResult = dataArray(obj)
     console.log(`dataResult: ${JSON.stringify(dataResult)}`)
@@ -141,7 +110,9 @@ module.exports = (db) => {
           .status(500)
           .json({ err: err.message });
       })
-      res.redirect(`/api/quizzes/${link}`);
+      res.render('postNewQuiz', {link})
+      //res.render('myquizzes');
+      //res.redirect(`/api/quizzes/${link}`);
       //res.json(':-)')
   })
   return router;
