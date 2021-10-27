bcd55d2bc9a6b06ad0a6c9e3a0ef9c05b69925a5
Akshatha Kulkarni <akshathakulkarni12@gmail.com>
2021-10-27 01:47:30 -0400

    Handle the data and insert them into db

--- routes/newQuiz.js
+++ routes/newQuiz.js
@@ -49,9 +49,6 @@ module.exports = (db) => {
   })
   router.post('/', (req, res) => {
     console.log("inside newQuizPost")
-    const title = req.body.title;
-    const author_id = req.session.user_id;
-    console.log('author_id = ', author_id)
     console.log('req = ', req.body)
 
     const dataArray = (obj) => {
@@ -71,50 +68,77 @@ module.exports = (db) => {
     let obj = req.body
     let dataResult = dataArray(obj)
     console.log(`dataResult: ${JSON.stringify(dataResult)}`)
-   // console.log(title, author_id, unlisted)
+    console.log('length = ', dataResult.length);
+
+    const title = req.body.title;
+    const author_id = req.session.user_id;
+    const unlisted = req.body.unlisted;
+    let question = '';
+    let correct = '';
+    let wrong1 = '';
+    let wrong2 = '';
+    let wrong3 = '';
+    console.log('author_id = ', author_id)
+    console.log(title, author_id, unlisted)
     const link = generateRandomString(6, '8qy3zi');
     console.log('link = ', link)
-    for (let eachObj of dataResult) {
-      for (let eachItem in eachObj) {
-        console.log(eachItem)
-        //console.log('1 = ', eachObj[eachItem][query])
-      }
-    }
+    const query1 =`INSERT INTO quizzes (title, author_id, unlisted, link) VALUES ($1, $2, $3, $4) RETURNING *;`;
+    const values1 = [title, author_id, unlisted, link];
+    db.query(query1, values1)
+      .then(data => {
+        const newQuiz = data.rows;
+        console.log('quizzes = ', data.rows)
+        const quiz_id = data.rows[0].id;
+        console.log('id = ', quiz_id);
+        console.log('result = ', newQuiz);
+        for (let eachObj of dataResult) {
+          for (let eachItem in eachObj) {
+            //console.log(eachItem)
+            if (eachItem === 'query') {
+              console.log('question = ', eachObj[eachItem]);
+              question = eachObj[eachItem];
+            }
+            if (eachItem === 'correct') {
+              console.log('correct = ', eachObj[eachItem]);
+              correct = eachObj[eachItem];
+            }
+            if (eachItem === 'wrong1') {
+              console.log('wrong1 = ', eachObj[eachItem]);
+              wrong1 = eachObj[eachItem];
+            }
+            if (eachItem === 'wrong2') {
+              console.log('wrong2 = ', eachObj[eachItem]);
+              wrong2 = eachObj[eachItem];
+            }
+            if (eachItem === 'wrong3') {
+              console.log('wrong3 = ', eachObj[eachItem]);
+              wrong3 = eachObj[eachItem];
+            }
+          }
+          const query2 = `INSERT INTO questions (quiz_id, query, correct_answer, wrong_1, wrong_2, wrong_3) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *;`;
+          const values2 = [quiz_id, question, correct, wrong1, wrong2, wrong3];
+          db.query(query2, values2)
+            .then(data => {
+              const quizQuestionAnswers = data.rows;
+              const question_id = data.rows[0].id;
+              console.log('2 = ', quizQuestionAnswers);
+              const query3 = `INSERT INTO quizzes_questions (quiz_id, question_id) VALUES ($1, $2) RETURNING *;`;
+              const values3 = [quiz_id, question_id];
+              db.query(query3, values3)
+                .then(data => {
+                  const quiz_question_ids = data.rows;
+                  console.log('ids 3 = ', quiz_question_ids)
+                })
 
-    //console.log('title : ', req.body)
-    //const query1 =`INSERT INTO quizzes (title, author_id, unlisted, link) VALUES ($1, $2, $3, $4) RETURNING *;`;
-    //const values1 = [title, author_id, unlisted, link];
-
-    // db.query(query1, values1)
-    //   .then(data => {
-    //     const newQuiz = data.rows;
-    //     const quiz_id = data.rows[0].id;
-    //     console.log('id = ', quiz_id)
-    //     console.log('result = ', newQuiz);
-    //     const query2 = `INSERT INTO questions (quiz_id, query, correct_answer, wrong_1, wrong_2, wrong_3) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *;`;
-    //     const values2 = [quiz_id, query, correct, wrong1, wrong2, wrong3];
-    //     db.query(query2, values2)
-    //       .then(data => {
-    //         const quizQuestionAnswers = data.rows;
-    //         const question_id = data.rows[0].id;
-    //         console.log('2 = ', quizQuestionAnswers);
-    //         const query3 = `INSERT INTO quizzes_questions (quiz_id, question_id) VALUES ($1, $2) RETURNING *;`;
-    //         const values3 = [quiz_id, question_id];
-    //         db.query(query3, values3)
-    //           .then(data => {
-    //             const quiz_question_ids = data.rows;
-    //             console.log('ids 3 = ', quiz_question_ids)
-    //             res.json({ quiz_question_ids });
-    //           })
-
-    //       })
-    //   })
-    //   .catch((err) => {
-    //     res
-    //       .status(500)
-    //       .json({ err: err.message });
-    //   })
-
+            })
+        }
+      })
+      .catch((err) => {
+        res
+          .status(500)
+          .json({ err: err.message });
+      })
+      res.redirect(`/api/quiz/${link}`);
   })
   return router;
 }
