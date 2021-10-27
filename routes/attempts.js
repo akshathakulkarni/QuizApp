eca58fb07ea23516182a3b2e0784967bd4bf0f69
stephkri <stephane.krims@hotmail.ca>
2021-10-27 21:24:02 +0000

    Copy myAttempts route into attempts.js file

--- routes/attempts.js
+++ routes/attempts.js
@@ -3,10 +3,25 @@ const router = express.Router();
 
 module.exports = (db) => {
   router.get('/', (req, res) => {
-    db.query(`SELECT * FROM attempts;`)
+    //console.log('Req session:', req.session);
+    //console.log('Req params:', req.params)
+    const userId = req.session.user_id;
+    console.log('myAttempts route has been reached');
+    console.log('author id = ', userId);
+    db.query(`SELECT attempts.id as attemptid, attempts.user_id, attempts.quiz_id, attempts.score, attempts.link as attemptlink, x.name as attemptName, quizzes.title, y.name as authorName, quizzes.link as quizlink,
+    (SELECT count(*) FROM quizzes_questions WHERE quiz_id = quizzes.id)
+    FROM attempts
+    JOIN users x ON x.id = user_id
+    JOIN quizzes ON quiz_id = quizzes.id
+    JOIN users y ON y.id = quizzes.author_id
+    WHERE x.id = $1;`, [userId])
       .then(data => {
-        const attempts = data.rows;
-        res.json({ attempts });
+        const attemptData = data.rows;
+        db.query('SELECT name FROM users WHERE id = $1', [userId])
+        .then(userData => {
+          const name = userData.rows[0].name;
+          res.render('myattempts', { 'attemptData': attemptData, 'name': name })
+        })
       })
       .catch((err) => {
         res
