fed1d12ed6a7d2cd875034a8a880ab138da41df8
stephkri <stephane.krims@hotmail.ca>
2021-10-28 07:31:07 +0000

    Delete commented out code

--- routes/login.js
+++ routes/login.js
@@ -11,26 +11,6 @@ module.exports = (db) => {
     console.log('inside post route')
     const email = req.body.email;
     const password = req.body.password;
-    //const hash = bcrypt.hashSync(password, 10);
-    //console.log(hash);
-    /*
-    db.query(`SELECT name, id FROM users WHERE email = $1 AND password = $2`, [email, hash])
-      .then((data) => {
-        const name = data.rows[0].name;
-        req.session.user_id = data.rows[0].id;
-        req.session.name = data.rows[0].name;
-        console.log('name = ', req.session.name)
-        console.log('id = ', req.session.user_id);
-        res.redirect('/');
-      })
-      .catch((err) => {
-        console.log('error catch');
-        res.status(500)
-          //.json({error: err.message})
-          .render('login', { invalid: true });
-      });
-  })
-  */
     db.query(`SELECT name, id, password FROM users WHERE email = $1`, [email])
     .then((data) => {
       const name = data.rows[0].name;
@@ -46,11 +26,9 @@ module.exports = (db) => {
       }
     })
     .catch((err) => {
-      console.log('error catch');
-      res.status(500);
+      res.status(500)
+         .json({error: err.message});
       console.log(err);
-        //.json({error: err.message})
-        //.render('login', { invalid: true });
     });
   })
   return router;
