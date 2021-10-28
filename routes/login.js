981f775328f8ac975d1bc6027be1faff4e95a087
stephkri <stephane.krims@hotmail.ca>
2021-10-28 07:25:42 +0000

    Still not working

--- routes/login.js
+++ routes/login.js
@@ -11,7 +11,9 @@ module.exports = (db) => {
     console.log('inside post route')
     const email = req.body.email;
     const password = req.body.password;
-    const hash = bcrypt.hashSync(password);
+    const hash = bcrypt.hashSync(password, 10);
+    console.log(hash);
+    /*
     db.query(`SELECT name, id FROM users WHERE email = $1 AND password = $2`, [email, hash])
       .then((data) => {
         const name = data.rows[0].name;
@@ -27,6 +29,28 @@ module.exports = (db) => {
           //.json({error: err.message})
           .render('login', { invalid: true });
       });
+  })
+  */
+    db.query(`SELECT name, id, password FROM users WHERE email = $1`, [email])
+    .then((data) => {
+      const name = data.rows[0].name;
+      const dbPass = data.rows[0].password;
+      if (bcrypt.compareSync(password, hash)) {
+        req.session.user_id = data.rows[0].id;
+        req.session.name = data.rows[0].name;
+        console.log('name = ', req.session.name)
+        console.log('id = ', req.session.user_id);
+        res.redirect('/');
+      } else {
+        res.render('login', { invalid: true });
+      }
+    })
+    .catch((err) => {
+      console.log('error catch');
+      res.status(500)
+        //.json({error: err.message})
+        .render('login', { invalid: true });
+    });
   })
   return router;
 }
