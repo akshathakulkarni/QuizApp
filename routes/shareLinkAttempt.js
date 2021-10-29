714324c0159e62723fdade6145260ce18674d358
stephkri <stephane.krims@hotmail.ca>
2021-10-29 06:26:49 +0000

    Add name parameter in sharing template variables

--- routes/shareLinkAttempt.js
+++ routes/shareLinkAttempt.js
@@ -8,7 +8,15 @@ module.exports = (db) => {
     console.log('req = ', req.params)
     const share_link = `localhost:8080/api/attempts/${link}`;
     console.log('link =', share_link)
-    res.render('shareLinks', { share_link })
+    if (req.session.user_id) {
+      db.query('SELECT name FROM users WHERE id = $1', [req.session.user_id])
+      .then(data => {
+        const userName = data.rows[0].name;
+        res.render('shareLinks', { share_link, 'name': userName });
+      })
+    } else {
+      res.render('shareLinks', { share_link, 'name': null });
+    }
   })
   return router;
 }
